3973fd9956a87e0a3625ee8563f9fab543b4e748
Fournext <sebastianzc04@gmail.com>
2025-06-03 18:27:29 -0400

    despliegue

--- aula-inteligente-back/apirestfull/settings.py
+++ aula-inteligente-back/apirestfull/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-m9$ia0f^o^_*w1%oi0dsaw#=nix2+tktmkkfmp7jv^dme)w)s1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.0.2.2', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- aula-inteligente-front/src/environments/environment.developments.ts
+++ aula-inteligente-front/src/environments/environment.developments.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  endpoint: 'http://localhost:8000/' //url para local
+  //endpoint: 'http://localhost:8000/' //url para local
+  endpoint: 'http://ec2-3-144-12-170.us-east-2.compute.amazonaws.com:8000/' //url para local
 }
